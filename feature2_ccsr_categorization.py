d1b117a0d89b0b09aff0b4181f3fe260afbff64b
Homen Shum <77596827+HomenShum@users.noreply.github.com>
2023-06-28 20:41:21 -0700

    Update feature2_ccsr_categorization.py

--- feature2_ccsr_categorization.py
+++ feature2_ccsr_categorization.py
@@ -1,38 +1,72 @@
-import pandas as pd
-import numpy as np
-from numpy.linalg import norm
 import openai
-from feature1_clinical_note_summarization import patient_note_analysis_output
+import json
 import streamlit as st
 
-##### Datasets ############################################################################################################
-ccsr_df_feather = pd.read_feather('datasets/DXCCSR/DXCCSR-Reference-File-v2023-1.feather')
+"""
+This program can take patient_id as input and return the corresponding patient's notes from the database.
+The notes can be used as input for the GPT model to generate an analysis that is easy to read and useful for categorization tasks.
+"""
 
 ##### Settings ############################################################################################################
+
 openai.api_key = st.secrets["openai_api_key"]
 
+
+system_prompt = """ You are an AI assistant specialized in biomedical topics. You are provided with a text description from a patient's screening notes. Analyze the patient's notes and provide information useful for physicians. Here are your instructions:
+
+                    - Highlight conditions, symptoms, medical history, and any other information that can be helpful to the physicians.
+
+                    - Keep in mind that grouping a large number of diseases into manageable categories for statistical analysis and reporting is useful for physicians. 
+
+                    - Ensure the conversation includes information such as the type of disease, cause, location in the body, and patient's age and sex. 
+
+                    - Avoid providing medical advice or diagnostic information. 
+
+                    - Ensure the output is in markdown bullet point format for clarity.
+
+                    - Encourage the user to consult a healthcare professional for advice."""
+
+
 ##### Functions ############################################################################################################
-def gpt3_embedding(content, engine='text-embedding-ada-002'):
-    content = content.encode(encoding='ASCII',errors='ignore').decode() 
-    response = openai.Embedding.create(input=content,engine=engine) 
-    vector = response['data'][0]['embedding']  
-    return vector
 
-def similarity(v1, v2):  # return dot product of two vectors
-    return np.dot(v1, v2)/(norm(v1)*norm(v2)) #dot product is a measure of similarity between two vectors
+def get_patient_notes(patient_id):
+    """Get the patient's notes"""
+    if patient_id == "123":
+        patient_notes = {
+            "patient_id":   patient_id,
+            "notes":"""     Hi doctor, I am a 26 year old male. I am 5 feet and 9 inches tall and weigh 255 pounds. 
+                            When I eat spicy food, I poop blood. Sometimes when I have constipation as well, 
+                            I poop a little bit of blood. I am really scared that I have colon cancer. 
+                            I do have diarrhea often. I do not have a family history of colon cancer. 
+                            I got blood tests done last night. Please find my reports attached.
+                    """
+        }
+    else:
+        return "Patient not found"
+    return json.dumps(patient_notes)
 
-def search_index(text, data, count=5):
-    vector = gpt3_embedding(text)  # get vector data for the question
-    scores = []
-    for i, content in zip(data, ccsr_df_feather['CCSR Category Description']):
-        score = similarity(vector, i)  # compare vector data for the question versus each embedding
-        scores.append({'content': content, 'score': score})  # create similarity scores for each document
-    ordered = sorted(scores, key=lambda d: d['score'], reverse=True)  # in scores list, sort 0th "score" data -to- last "score" data by highest to lowest
-    return ordered[:count]  # return top count documents
 
-##### Main #################################################################################################################
-input_text = patient_note_analysis_output
+def patient_note_analysis(user_prompt):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0613",  # use your actual model here
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
+        ],
+    )
+    return response["choices"][0]["message"]["content"]
 
-ccsr_categories_list = search_index(input_text, ccsr_df_feather['Embeddings'].tolist())
 
-# print(ccsr_categories_list)
+##### Main ############################################################################################################
+id_search   =   "123"
+user_prompt =   f"""Can you analyze patient {id_search}'s notes? \n
+                    {get_patient_notes(id_search)} \n
+                    Output in markdown bullet points as follows: \n
+                    **Patient information:** \n
+                    **Symptoms and conditions:** \n
+                    **Concerns:** \n
+                    **Medical history and investigations:** \n
+                    **Medical departments:** \n
+                """
+patient_note_analysis_output = patient_note_analysis(user_prompt)
+# print (patient_note_analysis_output)
